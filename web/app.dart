3b82f05277ca0c6afe5245da8c6b2674b7a7bc59
Victor Savkin <vic.savkin@gmail.com>
2013-11-10 12:56:45 -0500

    adds StartCallCtrl and wires it up

--- web/app.dart
+++ web/app.dart
@@ -2,6 +2,7 @@ library talk_to_me;
 
 import 'dart:html' as html;
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:angular/angular.dart';
 import 'package:angular/routing/module.dart';
@@ -12,21 +13,27 @@ part 'call_serializer.dart';
 part 'call_storage.dart';
 part 'app_route_initializer.dart';
 
+part 'lib/web_rtc_server.dart';
+part 'components/toggle_component.dart';
 part 'components/agenda_component.dart';
 part 'models/call.dart';
 part 'models/agenda_item.dart';
 part 'views/create_call_ctrl.dart';
 part 'views/show_call_ctrl.dart';
+part 'views/start_call_ctrl.dart';
 
 
 class TalkToMeApp extends Module {
   TalkToMeApp(){
     type(CreateCallCtrl);
     type(ShowCallCtrl);
+    type(StartCallCtrl);
     type(AgendaItemComponent);
     type(AgendaComponent);
     type(CallSerializer);
     type(CallStorage);
+    type(ToggleComponent);
+    type(WebRtcServer);
 
     type(RouteInitializer, implementedBy: AppRouteInitializer);
     factory(NgRoutingUsePushState, (_) => new NgRoutingUsePushState.value(false));
---
+++ web/views/start_call_ctrl.dart
@@ -0,0 +1,18 @@
+part of talk_to_me;
+
+@NgController(
+    selector: "[start-call-ctrl]",
+    publishAs: "ctrl"
+)
+class StartCallCtrl {
+  WebRtcServer server;
+  String offer;
+  String answer;
+
+  StartCallCtrl(this.server){
+    server.createOffer().then((_) => offer = _);
+  }
+
+  startCall(){
+  }
+}
