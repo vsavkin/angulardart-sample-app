7dae5f39834f57342eb8036fec322b4dac33e98a
Victor Savkin <vic.savkin@gmail.com>
2013-11-17 17:37:41 -0500

    adds a method to CallStorage returning all stored calls

--- web/call_storage.dart
+++ web/call_storage.dart
@@ -5,15 +5,17 @@ class CallStorage {
 
   CallStorage(this.serializer);
 
+  get all => _s.keys.fold([], (res, curr) => res..add(_fetch(curr)));
+
   store(Call call){
     var id = _getId(call);
     _s[id] = serializer.serialize(call, id);
     return id;
   }
 
-  find(String id) => _s.containsKey(id) ? serializer.deserialize(_s[id]) : null;
+  find(String id) => _s.containsKey(id) ? _fetch(id) : null;
 
   _getId(call) => call.id != null ? call.id  : new Uuid().v4();
-
+  _fetch(id) => serializer.deserialize(_s[id]);
   get _s => html.window.localStorage;
 }
