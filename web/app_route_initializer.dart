9e0fb50df5842465065997d81b4739756ce021f2
Victor Savkin <vic.savkin@gmail.com>
2013-11-17 17:38:37 -0500

    removes the join route

--- web/app_route_initializer.dart
+++ web/app_route_initializer.dart
@@ -4,7 +4,6 @@ class AppRouteInitializer implements RouteInitializer {
   init(Router router, ViewFactory view) {
     router.root
       ..addRoute(name: 'create', path: '/create', enter: view('views/create.html'))
-      ..addRoute(name: 'join', path: '/join', enter: view('views/join.html'))
       ..addRoute(name: 'show', path: '/:confId/show', enter: view('views/show.html'))
       ..addRoute(name: 'welcome', path: '/', enter: view('views/welcome.html'), defaultRoute: true);
   }
