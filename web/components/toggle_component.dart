a41d81701fb0204a0f70da488ec3062521cb8aca
Victor Savkin <vic.savkin@gmail.com>
2013-11-22 14:39:19 -0500

    refactors the toggle component to be more generic

--- web/components/toggle_component.dart
+++ web/components/toggle_component.dart
@@ -6,13 +6,6 @@ part of talk_to_me;
     publishAs: 'ctrl'
 )
 class ToggleComponent {
+  @NgTwoWay("toggle-property")
   bool open = false;
-
-  @NgAttr("openLabel")
-  String openLabel;
-
-  @NgAttr("closeLabel")
-  String closeLabel;
-
-  toggle() => open = !open;
 }
