b19711b653b326d1600256d13fdf1c88f8b4cf97
Victor Savkin <vic.savkin@gmail.com>
2013-11-22 14:43:16 -0500

    adds the call component

---
+++ web/components/call_component.dart
@@ -0,0 +1,10 @@
+part of talk_to_me;
+
+@NgComponent(
+    selector: 'call',
+    templateUrl: 'components/call.html',
+    publishAs: 'ctrl',
+    applyAuthorStyles: true
+)
+class CallComponent {
+}
