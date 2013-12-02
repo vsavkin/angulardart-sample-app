d8e5bd5299681bd6f11dd09ab372e1f0db363375
Victor Savkin <vic.savkin@gmail.com>
2013-12-02 16:58:13 -0500

    adds global http interceptors

---
+++ web/global_http_interceptors.dart
@@ -0,0 +1,26 @@
+part of talk_to_me;
+
+class GlobalHttpInterceptors {
+  static setup(Injector inj) =>
+    new GlobalHttpInterceptors(inj)..addGlobalAlertInterceptor();
+
+  Injector inj;
+  GlobalHttpInterceptors(this.inj);
+
+  addGlobalAlertInterceptor() =>
+    inj.get(HttpInterceptors)..add(_buildGlobalAlertInterceptor());
+
+  _buildGlobalAlertInterceptor() =>
+    new HttpInterceptor()
+      ..responseError = (error) {
+        final messages = inj.get(Messages);
+        messages.alert(_globalAlertMessage());
+
+        return new Future.error(error);
+      };
+
+  _globalAlertMessage(){
+    final loc = inj.get(LocationWrapper);
+    return "Something went wrong! <a href='${loc.location.pathname}'>Go to the main page.</a>";
+  }
+}
