c19ab51eb1d754fb730306eec50dc91d1783baff
Victor Savkin <vic.savkin@gmail.com>
2013-12-17 17:51:07 -0500

    adds unit tests for ReceiptItem

--- test/talk_to_me_test.dart
+++ test/talk_to_me_test.dart
@@ -3,8 +3,10 @@ library talk_to_me_test;
 import 'package:unittest/unittest.dart';
 import '../web/talk_to_me.dart';
 
-part 'src/parse_agenda_item_test.dart';
+part 'unit/parse_agenda_item_test.dart';
+part 'unit/agenda_item_test.dart';
 
 main(){
   testParseAgendaItem();
+  testAgendaItem();
 }
