be3c8119bc43db7c4e74974e1c3c5abc96f6de8e
Victor Savkin <vic.savkin@gmail.com>
2013-12-19 22:17:26 -0500

    adds tests for AgendaItemComponents

--- test/talk_to_me_test.dart
+++ test/talk_to_me_test.dart
@@ -3,6 +3,7 @@ library talk_to_me_test;
 import 'package:unittest/unittest.dart';
 import 'package:dartmocks/dartmocks.dart';
 import 'dart:async';
+import 'dart:html' as html;
 
 import '../web/talk_to_me.dart';
 import 'package:angular/angular.dart';
@@ -13,10 +14,12 @@ part 'unit/parse_agenda_item_test.dart';
 part 'unit/agenda_item_test.dart';
 part 'unit/create_call_ctrl_test.dart';
 part 'unit/users_repository_test.dart';
+part 'unit/agenda_item_component_test.dart';
 
 main(){
   testParseAgendaItem();
   testAgendaItem();
   testCreateCallCtrl();
   testUsersRepository();
+  testAgendaItemComponent();
 }
---
+++ test/unit/agenda_item_component_test.dart
@@ -0,0 +1,70 @@
+part of talk_to_me_test;
+
+loadTemplates(List<String> templates){
+  updateCache(template, response) => inject((TemplateCache cache) => cache.put(template, response));
+
+  final futures = templates.map((template) =>
+    html.HttpRequest.request("base/web/${template}", method: "GET").
+    then((_) => updateCache(template, new HttpResponse(200, _.response))));
+
+  return Future.wait(futures);
+}
+
+compileComponent(String html, Map scope, callback){
+  inject((TestBed tb) {
+    final s = tb.rootScope.$new();
+    scope.forEach((k,v) => s[k] = v);
+
+    final el = tb.compile(html);
+
+    Timer.run(expectAsync0(() {
+      tb.rootScope.$digest();
+      callback(el.shadowRoot);
+    }));
+  });
+}
+
+testAgendaItemComponent(){;
+  group("[AgendaItemComponent]", () {
+    setUp(setUpInjector);
+    tearDown(tearDownInjector);
+
+    solo_group("[swiching between modes]", () {
+      html() => '<agenda-item item="item" agenda="agenda"></agenda-item>';
+      scope() => {"item" : new AgendaItem("description", true, 1), "agenda" : new AgendaComponent()};
+
+      setUp((){
+        module((Module _) => _..type(TestBed)..type(AgendaItemComponent));
+        return loadTemplates(['components/agenda_item.html']);
+      });
+
+      test("defaults to the show mode", (){
+        compileComponent(html(), scope(), (shadowRoot){
+          expect(shadowRoot.query("agenda-item-input"), isNull);
+        });
+      });
+
+      test("switches to edit", (){
+        compileComponent(html(), scope(), (shadowRoot){
+          final switchBtn = shadowRoot.query("button.switch-to-edit");
+
+          switchBtn.click();
+
+          expect(shadowRoot.query("agenda-item-input"), isNotNull);
+        });
+      });
+
+      test("switches to show", (){
+        compileComponent(html(), scope(), (shadowRoot){
+          shadowRoot.query("button.switch-to-edit").click();
+
+          final cancelBtn = shadowRoot.query("button[type=reset]");
+
+          cancelBtn.click();
+
+          expect(shadowRoot.query("agenda-item-input"), isNull);
+        });
+      });
+    });
+  });
+}
