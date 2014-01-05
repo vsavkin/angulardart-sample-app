dca9bddd0c012fcc3d50a1efcc3102011f5e8ea4
Victor Savkin <vic.savkin@gmail.com>
2014-01-05 13:43:25 -0500

    removes MirrorsUsed

--- web/talk_to_me.dart
+++ web/talk_to_me.dart
@@ -4,20 +4,6 @@ import 'dart:html' as html;
 import 'dart:convert';
 import 'dart:async';
 
-@MirrorsUsed(targets: const[
-    'talk_to_me',
-    'angular',
-    'angular.core',
-    'angular.core.dom',
-    'angular.filter',
-    'angular.perf',
-    'angular.directive',
-    'angular.routing',
-    'angular.core.parser',
-    'perf_api',
-    html.NodeTreeSanitizer
-],
-override: '*')
 import 'dart:mirrors';
 
 import 'package:angular/angular.dart';
